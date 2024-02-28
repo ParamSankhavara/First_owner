c488a5c6292b8c16699c40d76970e4c500bb49aa
param@magentaconnect.com <param@magentaconnect.com>
2024-02-29 01:22:03 +0530

    login api

--- routes/login.py
+++ routes/login.py
@@ -3,6 +3,9 @@ from config.db import db1,engine
 from models.user import User
 from config.db import db1,engine
 from sqlalchemy import text
+from uuid import uuid4
+from models.session import Session
+from utiles.decorater import get_user_data,set_session
 
 login = APIRouter()
 
@@ -10,17 +13,29 @@ login = APIRouter()
 @login.post('/login')
 async def login_fuct(request : Request):
     json_data = await request.json()
-    for i in ['username','password']:
+    for i in ['mobile_no','password']:
         if i not in json_data.keys():
             return {"status":500,"message":f"{i} is missing","data":{}}
         if len(json_data[i]) == 0:
             return {"status":500,"message":f"{i} can not be empty","data":{}}
-    data = [row._asdict() for row in db1.execute(text("SELECT * FROM users")).all()]
-    print(data,flush=True)
-    return {"status":"done"}
+    user_data = get_user_data(json_data['mobile_no'],json_data['password'])
+    if len(user_data) == 0:
+        return {"status":200,"message":"Mobile no or password is wrong","data":{}}
+    user_data = user_data[0]
+    del user_data['_sa_instance_state']
+    rand_token = uuid4()
+    set_session(user_data['id'],rand_token)
+    user_data['token'] = rand_token
+    return {"status":200,"message":"Login success","data":user_data}
 
 @login.post('/logout')
 async def login_fuct(request : Request):
     json_data = await request.json()
-    print(json_data,flush=True)
-    return {"success":"done"}
+    for i in ['id','token']:
+        if i not in json_data.keys():
+            return {"status":500,"message":f"{i} is missing","data":{}}
+        if len(json_data[i]) == 0:
+            return {"status":500,"message":f"{i} can not be empty","data":{}}
+    db1.query(Session).filter(Session.user_id == json_data['id'],Session.token == json_data['token']).delete()
+    db1.commit()
+    return {"success":200,"message":"Logout success","data":{}}
---
+++ utiles/decorater.py
@@ -0,0 +1,20 @@
+from config.db import db1,engine
+from models.user import User
+from models.session import Session
+from config.db import db1,engine
+from sqlalchemy import text,and_
+
+def get_user_data(mobile_no,password):
+    data = [i.__dict__ for i in db1.query(User).filter(and_(User.mobile_no == mobile_no,User.password == password))]
+    return data
+
+
+def set_session(user_id,token):
+    data = [i.__dict__ for i in db1.query(Session).filter(Session.user_id == user_id)]
+    if len(data) == 0:
+        db1.add(Session(user_id=user_id,token=token))
+        db1.commit()
+    else:
+        db1.query(Session).filter(Session.user_id == user_id).update({Session.token:token})
+        db1.commit()
+    return None
