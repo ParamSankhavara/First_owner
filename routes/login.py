3a868e62ed8464796535e4c3d34cd42160ef6def
param@magentaconnect.com <param@magentaconnect.com>
2024-03-03 18:44:37 +0530

    add forget and change password api add validate request

--- routes/login.py
+++ routes/login.py
@@ -5,7 +5,8 @@ from config.db import db1,engine
 from sqlalchemy import text
 from uuid import uuid4
 from models.session import Session
-from utiles.decorater import get_user_data,set_session
+from utiles.decorater import get_user_data,set_session,validate_request
+from sqlalchemy import and_
 
 login = APIRouter()
 
@@ -26,7 +27,6 @@ async def login_fuct(request : Request):
     if len(user_data) == 0:
         return {"status":200,"message":"Mobile no or password is wrong","data":{}}
     user_data = user_data[0]
-    del user_data['_sa_instance_state']
     rand_token = uuid4()
     set_session(user_data['id'],rand_token)
     user_data['token'] = rand_token
@@ -43,3 +43,43 @@ async def login_fuct(request : Request):
     db1.query(Session).filter(Session.user_id == json_data['user_id']).delete()
     db1.commit()
     return {"success":200,"message":"Logout success","data":{}}
+
+
+
+@login.post('/change_password')
+@validate_request
+async def change_password(request : Request):
+    json_data = await request.json()
+    for i in ['user_id','old_password','new_password']:
+        if i not in json_data.keys():
+            return {"status":500,"message":f"{i} is missing","data":{}}
+        if type(json_data[i]) == int:
+            if json_data[i] == 0:
+                return {"status":500,"message":f"{i} can not be empty","data":{}}
+        else:
+            if len(json_data[i]) == 0:
+                return {"status":500,"message":f"{i} can not be empty","data":{}}
+    if len([i.__dict__ for i in db1.query(User).filter(and_(User.id == int(json_data['user_id']),User.password == json_data['old_password']))]) != 0:
+        db1.query(User).filter(User.id == int(json_data['user_id'])).update({User.password:json_data['new_password']})
+        db1.commit()
+        return {"status":200,"message":"Password changed successfully","data":{}}
+    else:
+        return {"status":500,"message":"Old password is wrong","data":{}}
+    
+
+@login.post('/forgot_password')
+@validate_request
+async def forgot_password(request : Request):
+    json_data = await request.json()
+    for i in ['user_id','new_password']:
+        if i not in json_data.keys():
+            return {"status":500,"message":f"{i} is missing","data":{}}
+        if type(json_data[i]) == int:
+            if json_data[i] == 0:
+                return {"status":500,"message":f"{i} can not be empty","data":{}}
+        else:
+            if len(json_data[i]) == 0:
+                return {"status":500,"message":f"{i} can not be empty","data":{}}
+    db1.query(User).filter(User.id == int(json_data['user_id'])).update({User.password:json_data['new_password']})
+    db1.commit()
+    return {"status":200,"message":"Password changed successfully","data":{}}
--- utiles/decorater.py
+++ utiles/decorater.py
@@ -3,9 +3,24 @@ from models.user import User
 from models.session import Session
 from config.db import db1,engine
 from sqlalchemy import text,and_
+from fastapi import Request
+from functools import wraps
+import functools
+
 
 def get_user_data(mobile_no,password,role_id):
-    data = [i.__dict__ for i in db1.query(User).filter(and_(User.mobile_no == mobile_no,User.password == password,User.role_id == role_id))]
+    data = [i._asdict() for i in db1.execute(text(f"""
+    SELECT 
+        u.*,sq.question_id,q.question,sq.answer
+    FROM
+        users AS u
+            LEFT JOIN
+        security_question AS sq ON u.id = sq.user_id
+            LEFT JOIN
+        questions AS q ON q.id = sq.question_id
+    WHERE
+        mobile_no = '{mobile_no}' AND password = '{password}' AND role_id = {int(role_id)}"""))]
+    # data = [i.__dict__ for i in db1.query(User).filter(and_(User.mobile_no == mobile_no,User.password == password,User.role_id == role_id))]
     return data
 
 
@@ -18,3 +33,13 @@ def set_session(user_id,token):
         db1.query(Session).filter(Session.user_id == user_id).update({Session.token:token})
         db1.commit()
     return None
+
+# Custom decorator to validate the token
+def validate_request(func):
+    @wraps(func)
+    async def wrapper(request: Request, *args, **kwargs):
+        json_data = await request.json()
+        if len([i.__dict__ for i in db1.query(Session).filter(and_(Session.token == json_data['token'],Session.user_id == json_data['user_id']))]) == 0:
+            return {"status":500,"message":"Invalid token","data":{}}
+        return await func(request, *args, **kwargs)
+    return wrapper
