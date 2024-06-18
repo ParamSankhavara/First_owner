a54f6587c72ede590ae91d4d08fea3aa2aa374b3
param@magentaconnect.com <param@magentaconnect.com>
2024-06-18 08:03:06 +0530

    h

--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, unique=True,autoincrement=True)
     role_id = Column(Integer)
     username = Column(String(10))
-    mobile_no = Column(Integer, unique=True)
+    mobile_no = Column(String(10))
     email = Column(String(50), unique=True)
     password = Column(String(50))
     updated_on = Column(DateTime)
